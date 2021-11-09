03df8e77d948c1c4709235cfc7d590fcc4ab32e1
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-11-09 10:53:14 +0000

    rearangement to make distutils work

--- include/CPM_include/CPM.h
+++ include/CPM_include/CPM.h
@@ -1,4 +1,14 @@
 /*
 
 Code of the Coarse-to-Fine PatchMatch, published at CVPR 2016 in
-"Efficient Coarse-to-Fine PatchMatc
+"Efficient Coarse-to-Fine PatchMatch for Large Displacement Optical Flow"
+by Yinlin.Hu, Rui Song and Yunsong Li.
+
+Email: huyinlin@gmail.com
+
+Version 1.2
+
+Copyright (C) 2016 Yinlin.Hu
+
+Usages:
+
