6a514bd9f0c16ca2cd8b9af9bcf4da001d65b1ab
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-12-03 15:06:12 +0000

    Japanese translation updates.

--- include/CPM_include/Image.h
+++ include/CPM_include/Image.h
@@ -106,3 +106,141 @@ public:
 
 	bool BoundaryCheck() const;
 	// function to move this image to another one
+	template <class T1>
+	void moveto(Image<T1>& image,int x,int y,int width=0,int height=0);
+
+	// function of basic image operations
+	virtual bool imresize(float ratio, InterType type = INTER_LINEAR);
+	template <class T1>
+	void imresize(Image<T1>& result, float ratio, InterType type = INTER_LINEAR) const;
+	void imresize(int dstWidth, int dstHeight, InterType type = INTER_LINEAR);
+	template <class T1>
+	void imresize(Image<T1>& result, int dstWidth, int dstHeight, InterType type = INTER_LINEAR) const;
+
+	template <class T1>
+	void upSampleNN(Image<T1>& result,int ratio) const;
+
+	// image IO's
+	virtual bool saveImage(const char* filename) const;
+	virtual bool loadImage(const char* filename);
+	virtual bool saveImage(ofstream& myfile) const;
+	virtual bool loadImage(ifstream& myfile);
+#ifndef _MATLAB
+	virtual bool imread(const char* filename);
+	virtual bool imwrite(const char* filename) const;
+	virtual bool imwrite(const char* filename,ImageIO::ImageType) const;
+	virtual void imshow(char* winname, int waittime = 1) const;
+	virtual void imagesc(char* winname, int waittime = 1) const;
+
+	//virtual bool imread(const QString& filename);
+	//virtual void imread(const QImage& image);
+
+	//virtual bool imwrite(const QString& filename,int quality=100) const;
+	//virtual bool imwrite(const QString& filename,ImageIO::ImageType imagetype,int quality=100) const;
+	//virtual bool imwrite(const QString& fileanme,T min,T max,int quality=100) const;
+#else
+	virtual bool imread(const char* filename) const {return true;};
+	virtual bool imwrite(const char* filename) const {return true;};
+#endif
+
+	// matches stored as x1 y1 x2 y2
+	template <class T1>
+	void DrawLines(T1* posPtr, int lineCnt, float b, float g, float r, int thickness = 1);
+
+	// change the color space to CIELab
+	void ToLab() const;
+
+	template <class T1>
+	void ToLab(Image<T1>& image) const;
+
+	template <class T1>
+	Image<T1> dx (bool IsAdvancedFilter=false) const;
+
+	template <class T1>
+	void dx(Image<T1>& image,bool IsAdvancedFilter=false) const;
+
+	template<class T1>
+	Image<T1> dy(bool IsAdvancedFilter=false) const;
+
+	template <class T1>
+	void dy(Image<T1>& image,bool IsAdvancedFilter=false) const;
+
+	template <class T1>
+	void dxx(Image<T1>& image) const;
+
+	template <class T1>
+	void dyy(Image<T1>& image) const;
+
+	template <class T1>
+	void dxy(Image<T1>& image) const;
+
+	template <class T1>
+	void laplacian(Image<T1>& image) const;
+
+	template <class T1>
+	void gradientmag(Image<T1>& image) const;
+
+	void GaussianSmoothing(float sigma,int fsize);
+
+	template <class T1>
+	void GaussianSmoothing(Image<T1>& image,float sigma,int fsize) const;
+
+	template <class T1>
+	void GaussianSmoothing_transpose(Image<T1>& image,float sigma,int fsize) const;
+
+	template <class T1>
+	void smoothing(Image<T1>& image,float factor=4);
+
+	template <class T1>
+	Image<T1> smoothing(float factor=4);
+
+	void smoothing(float factor=4);
+
+	void MedianFiltering(int fsize = 2);
+
+	// function for filtering
+	template <class T1>
+	void imfilter(Image<T1>& image,const float* filter,int fsize) const;
+
+	template <class T1,class T2>
+	void imfilter(Image<T1>& image,const Image<T2>& kernel) const;
+
+	template <class T1>
+	Image<T1> imfilter(const float* filter,int fsize) const;
+
+	template <class T1>
+	void imfilter_h(Image<T1>& image,float* filter,int fsize) const;
+
+	template <class T1>
+	void imfilter_v(Image<T1>& image,float* filter,int fsize) const;
+
+	template <class T1>
+	void imfilter_hv(Image<T1>& image,const float* hfilter,int hfsize,const float* vfilter,int vfsize) const;
+
+	template<class T1>
+	void imfilter_hv(Image<T1>& image,const Image<float>& hfilter,const Image<float>& vfilter) const;
+
+	// function for filtering transpose
+	template <class T1>
+	void imfilter_transpose(Image<T1>& image,const float* filter,int fsize) const;
+
+	template <class T1,class T2>
+	void imfilter_transpose(Image<T1>& image,const Image<T2>& kernel) const;
+
+	template <class T1>
+	Image<T1> imfilter_transpose(const float* filter,int fsize) const;
+
+	template <class T1>
+	void imfilter_h_transpose(Image<T1>& image,float* filter,int fsize) const;
+
+	template <class T1>
+	void imfilter_v_transpose(Image<T1>& image,float* filter,int fsize) const;
+
+	template <class T1>
+	void imfilter_hv_transpose(Image<T1>& image,const float* hfilter,int hfsize,const float* vfilter,int vfsize) const;
+
+	template<class T1>
+	void imfilter_hv_transpose(Image<T1>& image,const Image<float>& hfilter,const Image<float>& vfilter) const;
+
+	// function to desaturating
+	template <class T1>
