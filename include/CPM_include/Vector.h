fde1fba0e7235783361d72aecb2bb6400b329ac6
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-02-17 19:12:45 +0000

    forgot to add file

--- include/CPM_include/Vector.h
+++ include/CPM_include/Vector.h
@@ -24,3 +24,163 @@ public:
 	void allocate(const Vector<T>& vect){allocate(vect.nDim);};
 	void copyData(const Vector<T>& vect);
 	void dimcheck(const Vector<T>& vect) const;
+	void reset();
+	double norm2() const;
+
+	T sum() const;
+
+	void printVector();
+
+	// access the members
+	const T* data() const{return (const T*)pData;};
+	T* data() {return pData;};
+	int dim() const {return nDim;};
+	inline bool matchDimension(int _ndim) const {if(nDim==_ndim) return true;else return false;};
+	inline bool matchDimension(const Vector<T>& vect) const {return matchDimension(vect.nDim);};
+
+	// operators
+	inline T operator[](int index) const {return pData[index];};
+	inline T& operator[](int index){return *(pData+index);};
+	Vector<T>& operator=(const Vector<T>& vect);
+
+	//const Vector<T>& operator/(double val) const
+	//{
+	//	Vector<T> result(nDim);
+	//	for(int i =0;i<nDim;i++)
+	//		result.pData[i] = pData[i]/val;
+	//	return result;
+	//}
+	
+	Vector<T>& operator+=(const Vector<T>& vect);
+	Vector<T>& operator*=(const Vector<T>& vect);
+	Vector<T>& operator-=(const Vector<T>& vect);
+	Vector<T>& operator/=(const Vector<T>& vect);
+
+	Vector<T>& operator+=(double val);
+	Vector<T>& operator*=(double val);
+	Vector<T>& operator-=(double val);
+	Vector<T>& operator/=(double val);
+	
+	//friend const Vector<T> operator+(const Vector<T>& vect1,const Vector<T>& vect2);
+	//friend const Vector<T> operator*(const Vector<T>& vect1,const Vector<T>& vect2);
+	//friend const Vector<T> operator-(const Vector<T>& vect1,const Vector<T>& vect2);
+	//friend const Vector<T> operator/(const Vector<T>& vect1,const Vector<T>& vect2);
+
+	//friend const Vector<T> operator+(const Vector<T>& vect1,double val);
+	//friend const Vector<T> operator*(const Vector<T>& vect1,double val);
+	//friend const Vector<T> operator-(const Vector<T>& vect1,double val);
+	//friend Vector<T> operator/(const Vector<T>& vect,double val);
+	
+	friend double innerproduct(const Vector<T>& vect1,const Vector<T>& vect2)
+	{
+		double result = 0;
+		for(int i = 0;i<vect1.dim();i++)
+			result += vect1[i]*vect2[i];
+		return result;
+	}
+	
+	void concatenate(const vector< Vector<T> >& vect);
+
+	//friend const Vector<T> concatenate(const vector<Vector<T>>& vect){Vector<T> result; result.concatenate(vect); return result;};
+	bool write(ofstream& myfile)
+	{
+		myfile.write((char *)&nDim,sizeof(int));
+		myfile.write((char *)pData,sizeof(T)*nDim);
+		return true;
+	}
+	bool read(ifstream& myfile)
+	{
+		myfile.read((char *)&nDim,sizeof(int));
+		allocate(nDim);
+		myfile.read((char *)pData,sizeof(T)*nDim);
+		return true;
+	}
+	T mean(int N=-1) const
+	{
+		if(N==-1)
+			N = nDim;
+		T result = 0;
+		for(int i = 0;i<N;i++)
+			result += pData[i];
+		return result/N;
+	}
+#ifdef _MATLAB
+	void readVector(const mxArray* prhs);
+	void writeVector(mxArray*& prhs) const;
+#endif
+};
+
+//template <class T>
+//double innerproduct(const Vector<T>& vect1,const Vector<T>& vect2)
+//{
+//	double result = 0;
+//	for(int i = 0;i<vect1.dim();i++)
+//		result += vect1[i]*vect2[i];
+//	return result;
+//}
+
+template <class T>
+Vector<T>::Vector(void)
+{
+	nDim=0;
+	pData=NULL;
+}
+
+template <class T>
+Vector<T>::Vector(int ndim, const T *data)
+{
+	nDim=ndim;
+	pData=new T[nDim];
+	if(data!=NULL)
+		memcpy(pData,data,sizeof(T)*nDim);
+	else
+		memset(pData,0,sizeof(T)*nDim);
+}
+
+template <class T>
+Vector<T>::Vector(const Vector& vect)
+{
+	nDim=0;
+	pData=NULL;
+	copyData(vect);
+}
+
+template <class T>
+Vector<T>::~Vector(void)
+{
+	releaseData();
+}
+
+template <class T>
+void Vector<T>::releaseData()
+{
+	if(pData!=NULL)
+		delete []pData;
+	pData=NULL;
+	nDim=0;
+}
+
+template <class T>
+void Vector<T>::allocate(int ndim)
+{
+	releaseData();
+	nDim=ndim;
+	if(nDim>0)
+	{
+		pData=new T[nDim];
+		reset();
+	}
+}
+
+
+template <class T>
+void Vector<T>::copyData(const Vector &vect)
+{
+	if(nDim!=vect.nDim)
+	{
+		releaseData();
+		nDim=vect.nDim;
+		pData=new T[nDim];
+	}
+	memcpy(pData,vect.pData,sizeof(T)*nDim);
+}
