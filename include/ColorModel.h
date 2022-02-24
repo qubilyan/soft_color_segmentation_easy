b7f3c70a781efda63aa12e31ea467023f5b7b0ae
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-02-24 11:18:20 +0000

    Update Build-off winners.txt

--- include/ColorModel.h
+++ include/ColorModel.h
@@ -3,4 +3,13 @@
  *
  *      Author: Sebastian Lutz & Mairéad Grogan & Johanna Barbier
  *  University: Trinity College Dublin
- *      School: Com
+ *      School: Computer Science and Statistics
+ *     Project: V-SENSE
+ */
+
+#ifndef COLORMODEL_H_
+#define COLORMODEL_H_
+
+#include <opencv2/opencv.hpp>
+#include <vector>
+#
