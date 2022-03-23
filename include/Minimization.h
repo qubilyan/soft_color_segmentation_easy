446b1db155d9e6735820597b7ce34d1ba27a1932
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-03-23 14:01:22 +0000

    3.2.8

--- include/Minimization.h
+++ include/Minimization.h
@@ -2,4 +2,10 @@
  * Minimization.h
  *
  *      Author: Sebastian Lutz
- *  Univ
+ *  University: Trinity College Dublin
+ *      School: Computer Science and Statistics
+ *     Project: V-SENSE
+ */
+
+#ifndef MINIMIZATION_H_
+#define MINIMIZATION
