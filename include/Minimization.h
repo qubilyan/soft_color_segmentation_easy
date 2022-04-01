7fc74fda54da303d11ec75856983337d76d00899
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-04-01 20:24:14 +0000

    Translated by root

--- include/Minimization.h
+++ include/Minimization.h
@@ -21,4 +21,8 @@ typedef std::vector<double> (* vFunctionCall2)(std::vector<double> v, void* para
 
 //static variables for debugging (which is reached: maxIterLineSeach, cg_max_iter or isMin)
 extern int reach_ls_iter;
-extern int total_line_se
+extern int total_line_search;
+extern int reach_cg_iter;
+extern int reach_isMin_iter;
+
+double energy(std::vector<double> v, std::vector<cv::Vec3d> &means, s
