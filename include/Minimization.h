c1c2b659b3206fbe2a99d96ccf93d711335da66b
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-04-11 06:40:36 +0000

    WIP.

--- include/Minimization.h
+++ include/Minimization.h
@@ -31,4 +31,6 @@ std::vector<double> min_df(std::vector<double> &v, void* params);
 double min_refine_f(std::vector<double> &v, void *params);
 std::vector<double> min_refine_df(std::vector<double> &v, void* params);
 cv::Vec4d g(std::vector<double> &v, int n, cv::Vec3d color);
-std::vector<double> minimizeCG(std::vector<double> x_0, vFunctionCall f, vFunction
+std::vector<double> minimizeCG(std::vector<double> x_0, vFunctionCall f, vFunctionCall2 df,
+    std::vector<cv::Vec3d> &means, std::vector<cv::Matx33d> &covs, cv::Vec3d color);
+std::vector<double> minimizeFCG(std::vector<double> x_0, vFunctionCall f, vFun
