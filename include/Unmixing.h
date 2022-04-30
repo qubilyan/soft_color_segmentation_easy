8cbf7910cf985ceaf67d7c3f20df8324ae7d2c47
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-04-30 10:35:42 +0000

    UT fixes

--- include/Unmixing.h
+++ include/Unmixing.h
@@ -1,4 +1,8 @@
 /*
  * Unmixing.h
  *
- *  Created on: 9 
+ *  Created on: 9 Mar 2017
+ *      Author: Sebastian Lutz
+ *  University: Trinity College Dublin
+ *      School: Computer Science and Statistics
+ *     Project: V
