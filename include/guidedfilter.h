4b308dc27bbc20efd87a7c2a9c1ec80d340fe74e
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-05-24 07:06:17 +0000

    Linux 3.4.101

--- include/guidedfilter.h
+++ include/guidedfilter.h
@@ -1,4 +1,11 @@
 /*
  * guidedfilter.h
  *
- *  Create
+ *  Created on: 9 Mar 2017
+ *      Author: https://github.com/atilimcetin/guided-filter
+ */
+
+#ifndef GUIDED_FILTER_H
+#define GUIDED_FILTER_H
+
+#include <opencv2/o
