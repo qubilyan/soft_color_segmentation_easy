11bc8283e5f9b2934a5f55a505b45cb6f30fb81d
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-06-07 16:07:30 +0000

    chore(release): Prepare next development version: 7.4.0-SNAPSHOT

--- src/Minimization.cpp
+++ src/Minimization.cpp
@@ -305,3 +305,160 @@ std::vector<double> min_refine_df(std::vector<double> &v, void* params){
 	double diff_gt_alpha = 0.0, sum_squared = 0.0, sum_u1 = 0.0, sum_u2 = 0.0, sum_u3 = 0.0;
 	cv::Vec3d u;
 	for(size_t i = 0; i < n; i++){
+		alpha = v.at(i);
+		alphai_gt = gt_alpha.at(i);
+		u1 = v.at(3*i+n);
+		u2 = v.at(3*i+n+1);
+		u3 = v.at(3*i+n+2);
+		diff_gt_alpha += pow((alpha - alphai_gt),2);
+		sum_squared += pow(alpha,2);
+		sum_u1 += alpha*u1; //B_i * alpha_i
+		sum_u2 += alpha*u2; //G_i * alpha_i
+		sum_u3 += alpha*u3; //R_i * alpha_i
+	}
+	for(size_t i = 0; i < n; i++){
+		alpha = v.at(i);
+		alphai_gt = gt_alpha.at(i);
+		u1 = v.at(3*i+n);
+		u2 = v.at(3*i+n+1);
+		u3 = v.at(3*i+n+2);
+		u = cv::Vec3d(u1,u2,u3);
+		//Alphas - mg changed this so that the alpha values are not estimated in the final step - the filtered alphas are used instead - if they change, they may not add to 1 after optimisation.
+		//these are the rows that were removed:
+		/*************************
+		//dist = pow(cv::Mahalanobis(u, means.at(i), (covs.at(i)).inv()),2);
+		//sparse = 0;
+		//dot_prod = lambda.dot(dg_refine_a(v, means.size(), color, i, gt_alpha));
+		//g_norm = 0.5*p* (4*pow(sum_u1-color.val[0],3)*u1
+		//			+ 4*pow(sum_u2-color.val[1],3)*u2 
+		//			+ 4*pow(sum_u3-color.val[2],3)*u3
+		//			+ 4*(diff_gt_alpha)*(alpha - alphai_gt)); //mg checked
+		//grad_a = dist+sparse+dot_prod+g_norm;
+		//df.at(i) = grad_a;
+		/****************************/
+		df.at(i) = 0;
+		//Colors
+		//u1
+		dist = alpha*d_maha(means.at(i),covs.at(i),u,0);
+		dot_prod = lambda.dot(cv::Vec4d(dg_u(v, means.size(), color, i).val[0],0,0,0));
+		g_norm = 0.5*p*4*pow(sum_u1-color.val[0],3)*alpha;
+		grad_u1 = dist+dot_prod+g_norm;
+		df.at(3*i+n) = grad_u1;
+		//u2
+		dist = alpha*d_maha(means.at(i),covs.at(i),u,1);
+		dot_prod = lambda.dot(cv::Vec4d(0,dg_u(v, means.size(), color, i).val[1],0,0));
+		g_norm = 0.5*p*4*pow(sum_u2-color.val[1],3)*alpha;
+		grad_u2 = dist+dot_prod+g_norm;
+		df.at(3*i+n+1) = grad_u2;
+		//u3
+		dist = alpha*d_maha(means.at(i),covs.at(i),u,2);
+		dot_prod = lambda.dot(cv::Vec4d(0,0,dg_u(v, means.size(), color, i).val[2],0));
+		g_norm = 0.5*p*4*pow(sum_u3-color.val[2],3)*alpha;
+		grad_u3 = dist+dot_prod+g_norm;
+		df.at(3*i+n+2) = grad_u3;
+	}
+	// Apply box constraints
+	for(size_t i = 0; i < 4*n; i++){
+		if((df.at(i) < 0 && v.at(i) >= 1) || (df.at(i) > 0 && v.at(i) <= 0)){
+			df.at(i) = 0;
+		}
+	}
+	//mg added this to make sure that the alpha values don't change in the final refinement step - can rejig this code in future to make better
+	//for(size_t i = 0; i < n; i++){
+	//	df.at(i) = 0;
+	//}
+	return df;
+}
+
+
+
+
+/**
+ * Returns -v for input vector v
+ */
+std::vector<double> negate_v(std::vector<double> v){
+	std::vector<double> r(v.size());
+	for(unsigned int i = 0; i < v.size(); i++){
+		r.at(i) = -v.at(i);
+	}
+	return r;
+}
+
+/**
+ * The beta value for the polak-ribiere conjugate gradient descent
+ */
+double b_pr(std::vector<double> &a, std::vector<double> &b){
+	double r1 = 0.0, r2 = 0.0;
+	for(unsigned int i = 0; i < a.size(); i++){
+		r1 += a.at(i)*(a.at(i)-b.at(i));
+		r2 += b.at(i)*b.at(i);
+	}
+	if(r2 != 0){
+		return r1/r2;
+	} else {
+		return 0;
+	}
+}
+
+/**
+ * Make Input vector v a unit vector
+ */
+std::vector<double> make_unit(std::vector<double> v){
+	double l = 0.0;
+	std::vector<double> r(v.size());
+	for(double d : v){
+		l += pow(d,2);
+	}
+	double k = sqrt(l);
+	for(unsigned int i = 0; i < v.size(); i++){
+		if(k != 0){
+			r.at(i) = v.at(i)/k;
+		}
+	}
+	return r;
+}
+
+/**
+ * Clip a double value into range [0,1]
+ */
+double clip(double x){
+	if(x > 1){
+		return 1;
+	} else if(x < 0){
+		return 0;
+	} else {
+		return x;
+	}
+}
+
+/**
+ * Clip a vector into range [0,1]
+ */
+std::vector<double> clip(std::vector<double> &v){
+	for(unsigned int i = 0; i < v.size(); i++){
+		v.at(i) = (v.at(i)>1.0)?1.0:v.at(i);
+		v.at(i) = (v.at(i)<0.0)?0.0:v.at(i);
+	}
+	return v;
+}
+
+/**
+ * Linear transformation of two vectors and a scalar: a+c*b
+ */
+std::vector<double> lin_transform(std::vector<double> &a, std::vector<double> &b, double c){
+	std::vector<double> r(a.size());
+	for(unsigned int i = 0; i < a.size(); i++){
+		r.at(i) = a.at(i)+c*b.at(i);
+	}
+	return r;
+}
+
+/**
+ * Backtracking line-search
+ * Start direction x, search direction d
+ */
+double line_search(vFunctionCall f, std::vector<double> &x, std::vector<double> d, void* params)
+{
+
+	double in = f(x,params);
+	double a_k = .5;
