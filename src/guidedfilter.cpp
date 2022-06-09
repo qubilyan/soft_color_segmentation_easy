776d29d48c5e98c2e261522b99bfa2bde42f6095
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-06-09 21:59:42 +0000

    Char: ipmi_bt_sm, fix infinite loop

--- src/guidedfilter.cpp
+++ src/guidedfilter.cpp
@@ -111,3 +111,146 @@ cv::Mat GuidedFilterMono::filterSingleChannel(const cv::Mat &p) const
 
     cv::Mat mean_a = boxfilter(a, r);
     cv::Mat mean_b = boxfilter(b, r);
+
+    return mean_a.mul(I) + mean_b;
+}
+
+GuidedFilterColor::GuidedFilterColor(const cv::Mat &origI, int r, double eps) : r(r), eps(eps)
+{
+    cv::Mat I;
+    if (origI.depth() == CV_32F || origI.depth() == CV_64F)
+        I = origI.clone();
+    else
+        I = convertTo(origI, CV_32F);
+
+    Idepth = I.depth();
+
+    cv::split(I, Ichannels);
+
+    mean_I_r = boxfilter(Ichannels[0], r);
+    mean_I_g = boxfilter(Ichannels[1], r);
+    mean_I_b = boxfilter(Ichannels[2], r);
+
+    // variance of I in each local patch: the matrix Sigma in Eqn (14).
+    // Note the variance in each local patch is a 3x3 symmetric matrix:
+    //           rr, rg, rb
+    //   Sigma = rg, gg, gb
+    //           rb, gb, bb
+    cv::Mat var_I_rr = boxfilter(Ichannels[0].mul(Ichannels[0]), r) - mean_I_r.mul(mean_I_r) + eps;
+    cv::Mat var_I_rg = boxfilter(Ichannels[0].mul(Ichannels[1]), r) - mean_I_r.mul(mean_I_g);
+    cv::Mat var_I_rb = boxfilter(Ichannels[0].mul(Ichannels[2]), r) - mean_I_r.mul(mean_I_b);
+    cv::Mat var_I_gg = boxfilter(Ichannels[1].mul(Ichannels[1]), r) - mean_I_g.mul(mean_I_g) + eps;
+    cv::Mat var_I_gb = boxfilter(Ichannels[1].mul(Ichannels[2]), r) - mean_I_g.mul(mean_I_b);
+    cv::Mat var_I_bb = boxfilter(Ichannels[2].mul(Ichannels[2]), r) - mean_I_b.mul(mean_I_b) + eps;
+
+    // Inverse of Sigma + eps * I
+    invrr = var_I_gg.mul(var_I_bb) - var_I_gb.mul(var_I_gb);
+    invrg = var_I_gb.mul(var_I_rb) - var_I_rg.mul(var_I_bb);
+    invrb = var_I_rg.mul(var_I_gb) - var_I_gg.mul(var_I_rb);
+    invgg = var_I_rr.mul(var_I_bb) - var_I_rb.mul(var_I_rb);
+    invgb = var_I_rb.mul(var_I_rg) - var_I_rr.mul(var_I_gb);
+    invbb = var_I_rr.mul(var_I_gg) - var_I_rg.mul(var_I_rg);
+
+    cv::Mat covDet = invrr.mul(var_I_rr) + invrg.mul(var_I_rg) + invrb.mul(var_I_rb);
+
+    invrr /= covDet;
+    invrg /= covDet;
+    invrb /= covDet;
+    invgg /= covDet;
+    invgb /= covDet;
+    invbb /= covDet;
+}
+
+cv::Mat GuidedFilterColor::filterSingleChannel(const cv::Mat &p) const
+{
+    cv::Mat mean_p = boxfilter(p, r);
+
+    cv::Mat mean_Ip_r = boxfilter(Ichannels[0].mul(p), r);
+    cv::Mat mean_Ip_g = boxfilter(Ichannels[1].mul(p), r);
+    cv::Mat mean_Ip_b = boxfilter(Ichannels[2].mul(p), r);
+
+    // covariance of (I, p) in each local patch.
+    cv::Mat cov_Ip_r = mean_Ip_r - mean_I_r.mul(mean_p);
+    cv::Mat cov_Ip_g = mean_Ip_g - mean_I_g.mul(mean_p);
+    cv::Mat cov_Ip_b = mean_Ip_b - mean_I_b.mul(mean_p);
+
+    cv::Mat a_r = invrr.mul(cov_Ip_r) + invrg.mul(cov_Ip_g) + invrb.mul(cov_Ip_b);
+    cv::Mat a_g = invrg.mul(cov_Ip_r) + invgg.mul(cov_Ip_g) + invgb.mul(cov_Ip_b);
+    cv::Mat a_b = invrb.mul(cov_Ip_r) + invgb.mul(cov_Ip_g) + invbb.mul(cov_Ip_b);
+
+    cv::Mat b = mean_p - a_r.mul(mean_I_r) - a_g.mul(mean_I_g) - a_b.mul(mean_I_b); // Eqn. (15) in the paper;
+
+    return (boxfilter(a_r, r).mul(Ichannels[0])
+          + boxfilter(a_g, r).mul(Ichannels[1])
+          + boxfilter(a_b, r).mul(Ichannels[2])
+          + boxfilter(b, r));  // Eqn. (16) in the paper;
+}
+
+
+GuidedFilter::GuidedFilter(const cv::Mat &I, int r, double eps)
+{
+    CV_Assert(I.channels() == 1 || I.channels() == 3);
+
+    if (I.channels() == 1)
+        impl_ = new GuidedFilterMono(I, 2 * r + 1, eps);
+    else
+        impl_ = new GuidedFilterColor(I, 2 * r + 1, eps);
+}
+
+GuidedFilter::~GuidedFilter()
+{
+    delete impl_;
+}
+
+cv::Mat GuidedFilter::filter(const cv::Mat &p, int depth) const
+{
+    return impl_->filter(p, depth);
+}
+
+cv::Mat guidedFilter(const cv::Mat &I, const cv::Mat &p, int r, double eps, int depth)
+{
+    return GuidedFilter(I, r, eps).filter(p, depth);
+}
+
+/**
+ * Do the matte regularisation step from Aksoy et al. paper, section 3
+ * "Unmixing-Based Soft Color Segmentation for Image Manipulation" [2016]
+ * 1. Apply the guided filter to the current given a frame set of layers
+ *    following the guide image guide_img, (use current frame as guide image)
+ * 2. Regularise resulting alpha values so that they add up to one.
+ * Return the regularised layers
+ * */
+std::vector<cv::Mat> MatteRegularisation(int radius, cv::Mat guide_img, std::vector<cv::Mat> layers)
+{
+    int n = layers.size(); //number of colours in CM
+    std::vector<cv::Mat> filtered_layers; //filtered using guided filter
+    std::vector<cv::Mat> regularised_layers; //regularised using matte regularisation
+
+    cv::Mat sum_filtered_alphas(guide_img.rows, guide_img.cols, CV_32FC1, cvScalar(0.0));
+    int r = radius;
+    double eps = .001; 
+    eps *= 255 * 255;
+    for(size_t i = 0; i < n; i++){
+        //initialise
+        cv::Mat layer = layers.at(i);
+        cv::Mat layer_split[4];
+        cv::Mat ALayer8U, fLayer, guide_img8U;
+        cv::Mat ALayer(guide_img.size(),CV_32FC1); //for alpha layer
+        cv::Mat fLayer8U(guide_img.size(),CV_8UC1); 
+
+        cv::split(layer, layer_split); //split RGBA channels in layers
+        layer_split[3].convertTo(ALayer, CV_32FC1); //convert to float 
+        ALayer.convertTo(ALayer8U, CV_8UC1);//convert to uint
+        guide_img.convertTo(guide_img8U, CV_8UC3); //convert imput image to uint
+        cv::ximgproc::guidedFilter(guide_img8U,ALayer8U,fLayer8U,r,eps,-1); //filter uint alpha layer with uint input image (filtering uint to prevent negative alpha values/decimals)
+        fLayer8U.convertTo(fLayer, CV_32FC1);//convert filtered layer back to float so we can manipulate values so they add to 1 (or 255).
+        cv::addWeighted(sum_filtered_alphas,1.0,fLayer,1.0/255.0,0,sum_filtered_alphas); //sum all of the filtered alpha layers - this will be used to normalise 
+        filtered_layers.push_back(fLayer);
+    }	
+
+    // Regularize alpha values so that they add up to one
+    for(size_t i = 0; i < n; i++){
+        cv::Mat orig_layer = layers.at(i); //these is the original 4D layers - with colour and alpha info
+        cv::Mat fillayer = filtered_layers.at(i); // these layers are the new filtered 1D alpha layers
+        cv::Mat reg_alpha(guide_img.rows, guide_img.cols, CV_32FC1, cvScalar(0.0));
+        //regularise the alpha values so they all add to 1 (or 255, depending on scaling)
